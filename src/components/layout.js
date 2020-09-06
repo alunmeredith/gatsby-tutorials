6b1e5f9189dff376a88c2ffcc1868608e42829ad
Alun Meredith <alun.g.m@gmail.com>
2020-09-06 11:30:24 +0100

    Adding a sidebar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+import Sidebar from "./sidebar"
+import layoutStyle from "./layout.module.css"
 
 const ListLink = props => (
     <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -7,20 +9,30 @@ const ListLink = props => (
     </li>
 )
 
+const Header = props => (
+    <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+            {props.children}
+        </Link>
+    </header>
+)
+
+const Footer = props => (
+    <footer>
+        <u1 className={layoutStyle.footer}>
+            <ListLink to="/about">About</ListLink>
+            <ListLink to="/contact">Contact</ListLink>
+        </u1>
+    </footer>
+)
+
 export default function Layout({ children }) {
     return (
         <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-            <header style={{ marginBottom: `1.5rem` }}>
-                <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-                    <h3>MySweetSite</h3>
-                </Link>
-                <u1 style={{ listStyle: `none`, float: `right` }}>
-                    <ListLink to="/">Home</ListLink>
-                    <ListLink to="/about">About</ListLink>
-                    <ListLink to="/contact">Contact</ListLink>
-                </u1>
-            </header>
+            <Header><h3>MySweetSite</h3></Header>
+            <Sidebar></Sidebar>
             {children}
+            <Footer></Footer>
         </div>
     )
 }
